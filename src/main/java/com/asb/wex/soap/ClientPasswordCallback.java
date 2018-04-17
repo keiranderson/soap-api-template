b3e4d291431e4cd518672dcf50910ad5c0e129c8
keir.anderson <keir.anderson@l-kanderson.network.local>
2018-04-17 16:19:57 -0500

    Initial Studio commit

---
+++ src/main/java/com/asb/wex/soap/ClientPasswordCallback.java
@@ -0,0 +1,22 @@
+package com.asb.wex.soap;
+
+import java.io.IOException;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+
+import org.apache.ws.security.WSPasswordCallback;
+
+public class ClientPasswordCallback implements CallbackHandler {
+
+	public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+		for (int i = 0; i < callbacks.length; i++) {
+			WSPasswordCallback pc = (WSPasswordCallback) callbacks[i];
+			pc.setPassword("password");
+
+			if (pc.getUsage() == WSPasswordCallback.SIGNATURE || pc.getUsage() == WSPasswordCallback.DECRYPT) {
+			}
+		}
+	}
+}
